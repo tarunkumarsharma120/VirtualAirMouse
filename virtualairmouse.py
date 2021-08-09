de0fabc72553594d013f215b4036c56ff8ab02eb
Tarun Kumar <76935270+tarunkumarsharma120@users.noreply.github.com>
2021-08-09 12:28:50 +0530

    Add files via upload

---
+++ virtualairmouse.py
@@ -0,0 +1,133 @@
+import cv2
+import numpy as np
+import time
+import mediapipe as mp
+import pyautogui
+import math
+from win32api import GetSystemMetrics
+ws,hs=pyautogui.size()
+# ws=GetSystemMetrics(1)
+# hs=GetSystemMetrics(0)
+wc,hc=640, 480
+cap=cv2.VideoCapture(0)
+cap.set(3,wc)
+cap.set(4,hc)
+frameR=100
+pTime = 0
+smoothening = 7
+plocX, plocY = 0, 0
+clocX, clocY = 0, 0
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
+                                        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+        xList = []
+        yList = []
+        bbox = []
+        self.lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
+
+            if draw:
+                cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
+                              (0, 255, 0), 2)
+
+        return self.lmList, bbox
+
+    def fingersUp(self):
+        fingers = []
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        for id in range(1, 5):
+
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+
+        return fingers
+
+    def findDistance(self, p1, p2, img, draw=True,r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
+detector=handDetector(maxHands=1)
+while True:
+    success,img=cap.read()
+    img= detector.findHands(img)
+    li,bbox=detector.findPosition(img)
+
+    if len(li)!=0:
+        x1,y1 = li[8][1:]
+        x2,y2= li[12][1:]
+        fingers = detector.fingersUp()
+        cv2.rectangle(img, (frameR, frameR), (wc - frameR, hc - frameR),(255, 0, 255), 2)
+        if fingers[1]==1 and fingers[2]==0:
+            x3=np.interp(x1,(frameR,wc-frameR),(0,ws))
+            y3=np.interp(y1,(frameR,hc-frameR),(0,hs))
+            clocX = plocX + (x3 - plocX) / smoothening
+            clocY = plocY + (y3 - plocY) / smoothening
+            pyautogui.moveTo(x3,y3, duration=0.2)
+            cv2.circle(img,(x1,y1),15,(255,255,0),cv2.FILLED)
+            plocX, plocY = clocX, clocY
+        if fingers[1]==1 and fingers[2]==1:
+            length, img, lineInfo = detector.findDistance(8, 12, img)
+            print(length)
+            if length < 40:
+                cv2.circle(img, (lineInfo[4], lineInfo[5]),15, (0, 255, 0), cv2.FILLED)
+                pyautogui.click(clicks=2)
+
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow('Image',img)
+    cv2.waitKey(1)
